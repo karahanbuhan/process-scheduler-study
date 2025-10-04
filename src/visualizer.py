0852a0bfce8bc0012c2a192cb18842171700e33c
Karahan Buhan <contact@karahanbuhan.com>
2025-10-04 11:55:19 +0300

    Add average waiting time data in chart

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+from fractions import Fraction
 
 def visualize_results(schedule, waiting_times, avg_waiting_time, mode='gui'):
     """
-    Visualize the scheduling results with a Gantt Chart and print waiting times.
+    Visualize the scheduling results with a Gantt Chart and display waiting times.
     
     Args:
         schedule (list): List of dicts [{'process': str, 'start': int, 'end': int}, ...]
@@ -11,17 +12,23 @@ def visualize_results(schedule, waiting_times, avg_waiting_time, mode='gui'):
         mode (str): 'gui' for Matplotlib
     """
     if mode == 'gui':
-        fig, ax = plt.subplots(figsize=(10, 4))
-        for s in schedule:
-            ax.barh(s['process'], s['end'] - s['start'], left=s['start'], height=0.4)
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Processes')
-        ax.set_title('Gantt Chart')
-        plt.tight_layout()
-        plt.show()
+        # Kesir formatını hesapla
+        n, d = Fraction.from_float(avg_waiting_time).limit_denominator().as_integer_ratio()
         
-        # Waiting times ve ortalama yazdır
+        # Konsol çıktısı
         print("Waiting Times:")
         for pid, wt in waiting_times.items():
             print(f"{pid}: {wt}")
-        print(f"Average Waiting Time: {avg_waiting_time:.2f}")
+        print("Average WT is %s=%s/%s" % (avg_waiting_time, n, d))
+        
+        # Gantt Chart
+        fig, ax = plt.subplots(figsize=(10, 4))
+        for s in schedule:
+            ax.barh(s['process'], s['end'] - s['start'], left=s['start'], height=0.4)
+        
+        # AWT'yi grafik başlığına ekle
+        ax.set_title(f'Gantt Chart (Avg Waiting Time: {avg_waiting_time:.2f} = {n}/{d})')
+        ax.set_xlabel('Time')
+        ax.set_ylabel('Processes')
+        plt.tight_layout()
+        plt.show()
